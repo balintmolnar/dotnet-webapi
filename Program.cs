1433e47671e13002f5d125cb795da3a30594b51d
Bálint Molnár <baalint.molnar@gmail.com>
2026-02-24 13:50:36 +0100

    Created helm chart and added liveness and readiness probes to Web API

--- Program.cs
+++ Program.cs
@@ -1,16 +1,20 @@
 using Microsoft.EntityFrameworkCore;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(connectionString, name: "Postgres");
 
 var app = builder.Build();
 
@@ -30,5 +34,14 @@ if (app.Environment.IsDevelopment() || true)
 app.UseAuthorization();
 app.MapGet("/", () => "Hello World! The API is running.");
 app.MapControllers();
+
+// Liveness
+app.MapHealthChecks("/health/live", new HealthCheckOptions
+{
+    Predicate = _ => false
+});
+
+// Readiness
+app.MapHealthChecks("/health/ready");
 
 app.Run();
